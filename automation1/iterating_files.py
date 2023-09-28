e256752a7053e8e2d68a5a0e9249b7b488bce58e
Kashinath11 <bken25gourkhali@gmail.com>
2023-09-28 17:45:36 +0545

    iterating_files

---
+++ automation1/iterating_files.py
@@ -0,0 +1,9 @@
+with open('E:/google_python_os/automation1/spider.txt') as file:
+    for line in file:
+        print(line.upper())
+
+
+# output
+# THIS IS NORMAL SPDER.TXT FILE
+
+# THIS IS TESTING FILE 
