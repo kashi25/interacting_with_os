7b35dce3e4544af290c40605759aaf93351efec3
Kashinath11 <bken25gourkhali@gmail.com>
2023-09-28 22:13:11 +0545

    writing_file_added2

--- automation1/writing_files.py
+++ automation1/writing_files.py
@@ -3,3 +3,5 @@ with open("first.txt", "w") as file:
     
 # """What happens to the previous contents of a file when we open it using "w" ("write" mode)?"""
 # The old contents get deleted as soon as we open the file.
+
+# for more details https://docs.python.org/3/library/functions.html#open
