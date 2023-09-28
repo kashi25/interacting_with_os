95f9b310efefb94fe0efab9c735b637e1e13c634
Kashinath11 <bken25gourkhali@gmail.com>
2023-09-28 17:47:52 +0545

    changes_update

--- automation1/iterating_files.py
+++ automation1/iterating_files.py
@@ -1,9 +1,18 @@
-with open('E:/google_python_os/automation1/spider.txt') as file:
-    for line in file:
-        print(line.upper())
+# with open('E:/google_python_os/automation1/spider.txt') as file:
+#     for line in file:
+#         print(line.upper())
 
 
 # output
 # THIS IS NORMAL SPDER.TXT FILE
 
 # THIS IS TESTING FILE 
+
+
+with open('E:/google_python_os/automation1/spider.txt')as file:
+    for line in file:
+        print(line.strip().upper())
+        
+# the output of the code is 
+# THIS IS NORMAL SPDER.TXT FILE
+# THIS IS TESTING FILE
