e848c8a2870ef6525328748dabf4ae8f85d66479
Kashinath11 <bken25gourkhali@gmail.com>
2023-09-28 22:10:22 +0545

    writing_file_added

--- automation1/iterating_files.py
+++ automation1/iterating_files.py
@@ -13,6 +13,7 @@ with open('E:/google_python_os/automation1/spider.txt')as file:
     for line in file:
         print(line.strip().upper())
         
+        
 # the output of the code is 
 # THIS IS NORMAL SPDER.TXT FILE
 # THIS IS TESTING FILE
---
+++ automation1/writing_files.py
@@ -0,0 +1,5 @@
+with open("first.txt", "w") as file:
+    file.write("it was a dark and stromy night")
+    
+# """What happens to the previous contents of a file when we open it using "w" ("write" mode)?"""
+# The old contents get deleted as soon as we open the file.
