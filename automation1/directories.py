0c45b86ccb53868862c4f056181feae57e7a0576
Kashinath11 <bken25gourkhali@gmail.com>
2023-09-29 22:48:24 +0545

    file_exist2

---
+++ automation1/directories.py
@@ -0,0 +1,13 @@
+import os
+print(os.getcwd)
+
+# to make directory
+os.mkdir("new_dir")
+# to change directory
+os.chdir("new_dir")
+
+# to remove directory
+os.rmdir("new_dir")
+
+# to find the condent of directory
+os.listdir("new_dir")
