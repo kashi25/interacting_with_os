605787e1aacdebfb41311557f9844a8bf36b4141
Kashinath11 <bken25gourkhali@gmail.com>
2023-09-30 14:26:26 +0545

    test_update

--- csv_operation_python/generate_csv.py
+++ csv_operation_python/generate_csv.py
@@ -2,7 +2,9 @@ import csv
 
 hosts = [["wokstation.lcal", "192.168.25.46"], ["webserver.cloud", "10.2.5.6"]]
 with open('host.csv', 'w') as host_csv:
-    writer = csv.writer(host_csv)
-    writer.writerows(hosts)
+    varm = csv.writer(host_csv)
+    varm.writerow(hosts)
+    varm.writerows(hosts)
+    
 
     
--- csv_operation_python/test.py
+++ csv_operation_python/test.py
@@ -1,6 +1,13 @@
 import csv
 
-content = [["Kashi Bhattarai", "jhapa"], ["Ashok Bhattarai", "Kathmandu"]]
-with open('testing.csv', 'w') as test_csv:
-    writer = csv.writer(test_csv)
-    writer.writerows(content)
+content = [[24, 'Lal Badhur Tamang', "kyampa", 30000],[34, 'Hari Gadhal', "Kyampa",  27000], [56, 'Gopla Shunuwar', "dadaline", 34000]]
+with open('random.csv', 'w', newline='') as rand_csv:  # Open the file in write mode and specify newline=''
+    jhot = csv.writer(rand_csv)
+    header = ["age", "Name", "address", "salary"]
+    
+    # Write the header row
+    jhot.writerow(header)
+    
+    # Write the data rows
+    jhot.writerows(content)
+
