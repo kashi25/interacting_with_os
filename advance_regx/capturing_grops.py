e3d204f0383e604a076a606ea8fc0d23f86c68d6
Kashinath11 <bken25gourkhali@gmail.com>
2023-10-05 19:06:10 +0545

    advance_regx1

--- advance_regx/capturing_grops.py
+++ advance_regx/capturing_grops.py
@@ -5,3 +5,18 @@ print(result)
 # <re.Match object; span=(0, 13), match='Lovelace, Ada'>
 print(result.group())
 # Lovelace, Ada
+print(result[0])
+print(result[2])
+# Ada
+print("{} {}".format(result[2], result[1]))
+# Ada Lovelace
+
+def rearrange_name(name):
+    result = re.search(r"^(\w*), (\w*)$", name)
+    if result is None:
+        return name
+    return "{} {}",format(result[2], result[1])
+rearrange_name("Bhattarai Kashi")
+# rearrange_name("Bhattarai, Ashok")
+
+# ^([\w \.-]*), ([\w \.-]*)$
