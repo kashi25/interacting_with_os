4f7efce5c8b29b3549c1d0654a8a294214e1df9c
Kashinath11 <bken25gourkhali@gmail.com>
2023-10-07 15:31:10 +0545

    assignment_update

--- csv_operation_python/assignment (2).py
+++ csv_operation_python/assignment (2).py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
 import csv
 
 def read_employees(csv_file_location):
     csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
-    employee_file = csv.DictReader(open(csv_file_location), dialect='empDialect')
     employee_list = []
-    for data in employee_file:
-        employee_list.append(data)
+    
+    with open(csv_file_location, mode='r', encoding='utf-8') as file:
+        employee_file = csv.DictReader(file, dialect='empDialect')
+        for data in employee_file:
+            employee_list.append(data)
+    
     return employee_list
 
 employee_list = read_employees('/home/student-02-a1ec66a506c1/data/employees.csv')
@@ -16,12 +20,12 @@ def process_data(employee_list):
     department_data = {}
     
     for employee_data in employee_list:
-        department = employee_data['Department']
-        if department in department_data:
-            department_data[department] += 1
+        department_name = employee_data['Department']
+        if department_name in department_data:
+            department_data[department_name] += 1
         else:
-            department_data[department] = 1
-            
+            department_data[department_name] = 1
+    
     return department_data
 
 dictionary = process_data(employee_list)
@@ -33,5 +37,6 @@ def write_report(dictionary, report_file):
         for k in sorted(dictionary):
             f.write(str(k) + ':' + str(dictionary[k]) + '\n')
 
-# Replace '<report_file>' with the actual file path where you want to save the report.
-write_report(dictionary, 'report.txt')
+# Provide a valid file path for the report file, e.g., '/home/student-02-a1ec66a506c1/data/report.txt'
+report_file_path = '/home/student-02-a1ec66a506c1/data/report.txt'
+write_report(dictionary, report_file_path)
