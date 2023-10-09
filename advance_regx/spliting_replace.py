683cb5188cc89aca576f91ee0b97744b26e1d3e0
Kashinath11 <bken25gourkhali@gmail.com>
2023-10-09 09:17:03 +0545

    spliting_regx

---
+++ advance_regx/spliting_replace.py
@@ -0,0 +1,15 @@
+import re
+
+ba= re.split(r"[.?!]", "One sentence. Another one? And Last one!")
+print(ba)
+
+['One sentence', ' Another one', ' And Last one', '']
+
+aa= re.split(r"([.?!])", "One sentence. Another one? And Last one!")
+print(aa)
+# ['One sentence', '.', ' Another one', '?', ' And Last one', '!', '']
+
+
+kr=re.sub(r"[\w.%+-]+@[\w.-]+", "[REDACTED]", "Redeived an email from  xya@gmail.com")
+print(kr)
+# Redeived an email from  [REDACTED]
