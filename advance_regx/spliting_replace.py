9599a51b315dcf8403afb4b1d4bcc519a29383dd
Kashinath11 <bken25gourkhali@gmail.com>
2023-10-11 09:17:59 +0545

    advance_regex

--- advance_regx/spliting_replace.py
+++ advance_regx/spliting_replace.py
@@ -19,3 +19,26 @@ print(kr)
 kr = re.sub(r"^([\w .-]*), ([\w .-]*)$", r"\2 \1", "Lovelace, Ada")
 print(kr)
 # Ada Lovelace
+
+# The multi_vowel_words function returns all words with 3 or more consecutive vowels(a, e,i, o, u). Fill in the regular expression to do that
+
+import re
+def multi_vowel_words(text):
+  pattern = r'\w*[aeiouAEIOU]{3,}\w*'
+  result = re.findall(pattern, text)
+  return result
+
+print(multi_vowel_words("Life is beautiful")) 
+# ['beautiful']
+
+print(multi_vowel_words("Obviously, the queen is courageous and gracious.")) 
+# ['Obviously', 'queen', 'courageous', 'gracious']
+
+print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner.")) 
+# ['rambunctious', 'quietly', 'delicious']
+
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)")) 
+# ['queue']
+
+print(multi_vowel_words("Hello world!")) 
+# []
